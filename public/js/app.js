9621815d3313af0cfa2c27450b6c76d04b7097f5
github username <github email address>
2019-08-08 22:16:41 -0400

    fetch from json

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
 console.log("Cloent side server")
+
+fetch('http://puzzle.mead.io/puzzle')
+.then(resp => resp.json())
+.then(data => {
+    console.log(data)
+})
+ 
+
+
+fetch('http://localhost:3000/weather?address=new_york')
+.then(resp => resp.json())
+.then(data => {
+    // console.log(data.error)
+    if(!data.error){
+        console.log(data.forecast.summary)
+        console.log(data.location)
+    }
+    else{
+        console.log(data.error)
+    }   
+})
+
