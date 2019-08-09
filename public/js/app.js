b2d84fe6fef35a7f2f9491005c5facbd03793718
github username <github email address>
2019-08-08 22:43:10 -0400

    form input. page render

--- public/js/app.js
+++ public/js/app.js
@@ -7,17 +7,28 @@ fetch('http://puzzle.mead.io/puzzle')
 })
  
 
+const weatherForm = document.querySelector("form")
+const searchBar = document.querySelector("input")
+const messageOne = document.querySelector("#message-1")
+const messageTwo = document.querySelector("#message-2")
 
-fetch('http://localhost:3000/weather?address=new_york')
-.then(resp => resp.json())
-.then(data => {
-    // console.log(data.error)
-    if(!data.error){
-        console.log(data.forecast.summary)
-        console.log(data.location)
-    }
-    else{
-        console.log(data.error)
-    }   
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    messageOne.innerText = "Loading..."
+    const location = searchBar.value 
+    fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(resp => resp.json())
+    .then(data => {
+        // console.log(data.error)
+        if(!data.error){
+            console.log(data.forecast.summary)
+            console.log(data.location)
+            messageOne.innerText = data.forecast.summary
+            messageTwo.innerText = data.location
+        }
+        else{
+            console.log(data.error)
+            messageOne.innerText = data.error
+        }   
+    })
 })
-
